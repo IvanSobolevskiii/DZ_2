bbb58fc6372a065b8abc3c0f5381168b65fad218
golden lira <empirecheat17@gmail.com>
2024-02-13 02:49:42 +0300

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = 29;
+
+if (number >= 10 && number <= 99)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if (firstDigit >= secondDigit)
+    {
+        Console.WriteLine($"В числе {number} цифра {firstDigit} является наибольшей");
+    }
+    else
+    {
+        Console.WriteLine($"В числе {number} цифра {secondDigit} является наибольшей");
+    }
+}
+else
+{
+    Console.WriteLine($"Число {number} не соответсвует требования (целое число из отрезка [10, 99])");
+}
