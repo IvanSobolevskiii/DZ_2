93c85b7fd4c68885792b8ef757db7be495ac17ff
golden lira <empirecheat17@gmail.com>
2024-02-13 02:20:39 +0300

    Add Task_2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int number = 332;
+
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратное 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не крастное 7 и 23");
+}
